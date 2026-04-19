d22ed73f9575a5ff3bb26a172eecdc39eea72f2e
Oliver Forral <intrepion@gmail.com>
2026-04-19 10:15:27 -0600

    Define prism-net-layout Flutter contracts

--- workspace/lib/contracts/prism_net_layout_item.dart
+++ workspace/lib/contracts/prism_net_layout_item.dart
@@ -1,1 +1,17 @@
+class PrismNetLayoutItem {
+  final String faceName;
+  final int x;
+  final int y;
+  final int displayWidth;
+  final int displayHeight;
+  final bool isHidden;
 
+  const PrismNetLayoutItem({
+    required this.faceName,
+    required this.x,
+    required this.y,
+    required this.displayWidth,
+    required this.displayHeight,
+    required this.isHidden,
+  });
+}
