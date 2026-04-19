837e2ae3216f1063c1f0d274eb66fe22e33eb735
Oliver Forral <intrepion@gmail.com>
2026-04-19 10:26:22 -0600

    3. Red: Size The Canonical Faces

--- workspace/test/code/prism_net_layout_service_test.dart
+++ workspace/test/code/prism_net_layout_service_test.dart
@@ -9,4 +9,31 @@ void main() {
     expect(dimensions.height, 360);
     expect(dimensions.depth, 90);
   });
+
+  test('faceDisplaySize returns width x height for front', () {
+    final dimensions = buildPrismDimensions(width: 240, height: 360, depth: 90);
+
+    final size = faceDisplaySize('front', dimensions);
+
+    expect(size.width, 240);
+    expect(size.height, 360);
+  });
+
+  test('faceDisplaySize returns depth x height for left', () {
+    final dimensions = buildPrismDimensions(width: 240, height: 360, depth: 90);
+
+    final size = faceDisplaySize('left', dimensions);
+
+    expect(size.width, 90);
+    expect(size.height, 360);
+  });
+
+  test('faceDisplaySize returns width x depth for top', () {
+    final dimensions = buildPrismDimensions(width: 240, height: 360, depth: 90);
+
+    final size = faceDisplaySize('top', dimensions);
+
+    expect(size.width, 240);
+    expect(size.height, 90);
+  });
 }
