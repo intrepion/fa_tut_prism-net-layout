47a6c31881d3c6b42018708ad4f8dbd74c055193
Oliver Forral <intrepion@gmail.com>
2026-04-19 10:23:42 -0600

    1. Red: Preserve The Prism Dimensions

--- workspace/test/code/prism_net_layout_service_test.dart
+++ workspace/test/code/prism_net_layout_service_test.dart
@@ -1,1 +1,12 @@
+import 'package:prism_net_layout/code/prism_net_layout_service.dart';
+import 'package:test/test.dart';
 
+void main() {
+  test('buildPrismDimensions preserves width height and depth', () {
+    final dimensions = buildPrismDimensions(width: 240, height: 360, depth: 90);
+
+    expect(dimensions.width, 240);
+    expect(dimensions.height, 360);
+    expect(dimensions.depth, 90);
+  });
+}
