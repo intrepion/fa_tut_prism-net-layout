86735fa6272c5eb536cf65ef0dbe0be7048330f7
Oliver Forral <intrepion@gmail.com>
2026-04-19 10:32:18 -0600

    3. Red: Add The Integration Test

--- workspace/integration_test/app_test.dart
+++ workspace/integration_test/app_test.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:prism_net_layout/adapter/prism_net_layout_page.dart';
 
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('renders the prism net layout title', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: PrismNetLayoutPage()));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Prism Net Layout'), findsOneWidget);
+  });
+}
